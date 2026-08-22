b1e536cb9839a3368226f1f5fee2bc2ba8b2337c
Ugo Evola <ugoevola@gmail.com>
2026-08-22 03:05:33 +0200

    fix: alias errorMessage on the attribute that exists
    IsUuid, IsTime and IsStringNumber declared @AliasFor on an attribute named
    "message", which IsJsonValidation does not have. Spring raises an
    AnnotationConfigurationException as soon as a merged annotation lookup walks a
    property carrying one of them, which makes the AOT processing of any API using
    them fail.

--- src/main/kotlin/org/uevola/jsonautovalidation/api/annotations/rules/IsUuid.kt
+++ src/main/kotlin/org/uevola/jsonautovalidation/api/annotations/rules/IsUuid.kt
@@ -7,6 +7,6 @@ import org.springframework.core.annotation.AliasFor
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
 annotation class IsUuid(
-    @get:AliasFor(annotation = IsJsonValidation::class, attribute = "message")
+    @get:AliasFor(annotation = IsJsonValidation::class, attribute = "errorMessage")
     val errorMessage: String = "The field @{fieldName} must be a valid UUID."
 )
